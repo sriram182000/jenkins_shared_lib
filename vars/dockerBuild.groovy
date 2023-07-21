5aeab03ab38d60897df8492a4c48aec52cb6c2d0
Sriram <saisriram13@gmail.com>
2023-07-21 18:11:57 +0530

    new start

--- vars/dockerBuild.groovy
+++
@@ -1,7 +0,0 @@
-def call(String ImgName, String ImgTag, String DockerHubUser) {
-    sh """
-     docker image build -t ${DockerHubUser}/${ImgName} .
-     docker image tag  ${DockerHubUser}/${ImgName} ${DockerHubUser}/${ImgName}:${ImgTag}
-     docker image tag  ${DockerHubUser}/${ImgName} ${DockerHubUser}/${ImgName}:latest
-    """
-}
