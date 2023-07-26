c5cf96d1ea99b8707e3c905a3f8f2114c384e8f5
Sriram <saisriram13@gmail.com>
2023-07-26 12:18:01 +0530

    added docker build step

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,5 +1,6 @@
 def call(String Dockerhub, String Imgname, String Tagname) {
     sh """
+    docker service docker start
     docker image build -t ${Dockerhub}/${Imgname}:${Tagname} .
     docker image tag ${Dockerhub}/${Imgname} ${Dockerhub}/${Imgname}:${Tagname} 
     docker image tag ${Dockerhub}/${Imgname} ${Dockerhub}/${Imgname}:latest
